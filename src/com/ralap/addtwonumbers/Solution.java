0b54b99a8ce116df1f4a8567812ee494d6dedbdd
ralaphao <ralaphao@tencent.com>
2022-02-16 20:29:01 +0800

    fix: bug

--- src/com/ralap/addtwonumbers/Solution.java
+++ src/com/ralap/addtwonumbers/Solution.java
@@ -15,11 +15,35 @@ public class Solution {
 
     public static void main(String[] args) {
         Solution solution = new Solution();
-        int[] resultHash = solution.addTwoNumbers(new int[]{1, 2, 3, 5, 7, 9, 11}, 13);
+
+        int[] resultHash = solution.addTwoNumbers();
         Arrays.stream(resultHash).forEach(System.out::println);
     }
 
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-
+        ListNode head = null, tail = null;
+        int carry = 0;
+        while (l1 != null || l2 != null) {
+            int n1 = l1 != null ? l1.val : 0;
+            int n2 = l2 != null ? l2.val : 0;
+            int sum = n1 + n2 + carry;
+            if (head == null) {
+                head = tail = new ListNode(sum % 10);
+            } else {
+                tail.next = new ListNode(sum % 10);
+                tail = tail.next;
+            }
+            carry = sum / 10;
+            if (l1 != null) {
+                l1 = l1.next;
+            }
+            if (l2 != null) {
+                l2 = l2.next;
+            }
+        }
+        if (carry > 0) {
+            tail.next = new ListNode(carry);
+        }
+        return head;
     }
 }
