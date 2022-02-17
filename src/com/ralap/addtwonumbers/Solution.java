ac2d66e7c9a5fad34c5c0934993678cdc594e048
ralaphao <ralaphao@tencent.com>
2022-02-17 21:09:32 +0800

    fix: add

--- src/com/ralap/addtwonumbers/Solution.java
+++ src/com/ralap/addtwonumbers/Solution.java
@@ -18,7 +18,6 @@ public class Solution {
         ListNode l1 = new ListNode(9, new ListNode(9, new ListNode(9, new ListNode(9))));
         ListNode l2 = new ListNode(9, new ListNode(9));
 
-
         ListNode result = solution.addTwoNumbers(l1, l2);
         while (result != null) {
             System.out.println(result.val);
@@ -28,26 +27,31 @@ public class Solution {
 
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
         ListNode head = null, tail = null;
+        // 进位
         int carry = 0;
+        // 链表全部为空,结束
         while (l1 != null || l2 != null) {
             int var1 = l1 == null ? 0 : l1.val;
             int var2 = l2 == null ? 0 : l2.val;
             int sum = var1 + var2 + carry;
             if (head == null) {
+                // 首次初始化节点
                 head = tail = new ListNode(sum % 10);
             } else {
+                // 赋值并后移
                 tail.next = new ListNode(sum % 10);
                 tail = tail.next;
             }
             carry = sum / 10;
+            // 后移指针
             if (l1 != null) {
                 l1 = l1.next;
             }
-
             if (l2 != null) {
                 l2 = l2.next;
             }
         }
+        // 最高位是否有进位
         if (carry != 0) {
             tail.next = new ListNode(carry);
         }
