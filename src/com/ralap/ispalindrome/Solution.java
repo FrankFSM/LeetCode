99ac20b4425ce7864c9a4742b5b48916cec187ee
RalapHao <hao_jixiang@163.com>
2022-02-20 19:39:11 +0800

    feat: 回文数

---
+++ src/com/ralap/ispalindrome/Solution.java
@@ -0,0 +1,57 @@
+package com.ralap.ispalindrome;
+
+
+/**
+ * 9.回文数
+ * 给你一个整数 x ，如果 x 是一个回文整数，返回 true ；否则，返回 false 。
+ * <p>
+ * 回文数是指正序（从左向右）和倒序（从右向左）读都是一样的整数。
+ * <p>
+ * 例如，121 是回文，而 123 不是。
+ */
+public class Solution {
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        boolean result = solution.isPalindrome(123);
+        System.out.println(result);
+
+    }
+
+    /**
+     * 时间复杂度O(logn)
+     * 空间复杂度O(1)
+     */
+    public boolean isPalindrome(int x) {
+//        超时
+//        if(x<0)
+//            return false;
+//        // 负数直接返回
+//        String str = Integer.toString(x);
+//        int head = 0, tail = str.length()-1;
+//        while (tail >= head) {
+//            if (str.charAt(tail) != str.charAt(head)) {
+//                return false;
+//            }
+//        }
+
+        // 负数、10的倍数（除去0）
+        // 直接返回 false
+        if (x < 0 || (x % 10 == 0 && x != 0)) {
+            return false;
+        }
+
+        // 翻转的值
+        int revertNumber = 0;
+
+        // 中位数分割值对比
+        while (x > revertNumber) {
+            // 翻转值
+            revertNumber = revertNumber * 10 + x % 10;
+            // 正常值
+            x /= 10;
+        }
+        // 偶数相等，基数去除中位数
+        return x == revertNumber || revertNumber /10 == x;
+    }
+}
--- src/com/ralap/lengthoflongestsubstring/Solution.java
+++ src/com/ralap/lengthoflongestsubstring/Solution.java
@@ -1,8 +1,6 @@
 package com.ralap.lengthoflongestsubstring;
 
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 /**
  * 2.无重复字符的最长子串
@@ -12,24 +10,35 @@ public class Solution {
 
     public static void main(String[] args) {
         Solution solution = new Solution();
-        int result = solution.lengthOfLongestSubstring("abcabcbb");
-
+        int result = solution.lengthOfLongestSubstring("abcdabcbb");
         System.out.println(result);
 
     }
 
     /**
-     * 时间复杂度O(N^2)
-     * 空间复杂度O(1)
+     * 滑动窗口
+     * 时间复杂度O(N)
+     * 空间复杂度O(|Σ|) Σ所有字符集
      */
     public int lengthOfLongestSubstring(String s) {
-        int head = 0 , tail = 0 , maxLength = 0;
-        while (tail == s.length()){
+        int tail = -1, maxLength = 0;
+        int sLen = s.length();
+        Set<Character> str = new HashSet<>(sLen);
 
+        for (int i = 0; i < sLen; i++) {
+            if (i != 0) {
+                // 移除第一个重复的元素
+                str.remove(s.charAt(i - 1));
+            }
+            // 判断是否存在重复的元素
+            while (tail < sLen - 1 && !str.contains(s.charAt(tail +1))) {
+                // 无重复，加入集合
+                str.add(s.charAt(tail + 1));
+                // 指针后移
+                ++tail;
+            }
+            maxLength = Math.max(maxLength, str.size());
         }
         return maxLength;
-
-
-
     }
 }
