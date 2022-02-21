28f3aaa861d74cea18867160008c1f3159fba331
ralaphao <ralaphao.tencent.com>
2022-02-21 20:04:13 +0800

    feat: romanToInt

--- src/com/ralap/ispalindrome/Solution.java
+++ src/com/ralap/ispalindrome/Solution.java
@@ -4,9 +4,7 @@ package com.ralap.ispalindrome;
 /**
  * 9.回文数
  * 给你一个整数 x ，如果 x 是一个回文整数，返回 true ；否则，返回 false 。
- * <p>
  * 回文数是指正序（从左向右）和倒序（从右向左）读都是一样的整数。
- * <p>
  * 例如，121 是回文，而 123 不是。
  */
 public class Solution {
@@ -15,7 +13,6 @@ public class Solution {
         Solution solution = new Solution();
         boolean result = solution.isPalindrome(123);
         System.out.println(result);
-
     }
 
     /**
