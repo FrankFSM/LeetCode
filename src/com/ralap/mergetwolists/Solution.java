5cb8d77e6a91d171eef4b023d606f2947d687db0
RalapHao <hao_jixiang@163.com>
2022-02-26 11:17:11 +0800

    fix: merge two lists more solutions

--- src/com/ralap/mergetwolists/Solution.java
+++ src/com/ralap/mergetwolists/Solution.java
@@ -12,11 +12,58 @@ public class Solution {
         Solution solution = new Solution();
         ListNode l1 = new ListNode(1, new ListNode(2, new ListNode(4, new ListNode(5))));
         ListNode l2 = new ListNode(1, new ListNode(3));
-        ListNode result = solution.mergeTwoLists(l1, l2);
+//        ListNode result = solution.mergeTwoLists(l1, l2);
+//        ListNode result = solution.mergeTwoListRecursion(l1, l2);
+        ListNode result = solution.mergeTwoListsLoop(l1, l2);
+
+
         while (result != null) {
             System.out.println(result.val);
             result = result.next;
         }
+    }
+
+    /**
+     * 递归
+     * 时间复杂度： O(N+M)
+     * 空间复杂度： O(N+M)
+     */
+    public ListNode mergeTwoListRecursion(ListNode l1, ListNode l2) {
+        if (l1 == null) {
+            return l2;
+        } else if (l2 == null) {
+            return l1;
+        } else if (l1.val < l2.val) {
+            l1.next = mergeTwoListRecursion(l1.next, l2);
+            return l1;
+        } else {
+            l2.next = mergeTwoListRecursion(l1, l2.next);
+            return l2;
+        }
+
+    }
+
+    /**
+     * 循环
+     * 时间复杂度：O(N+M)
+     * 空间复杂度：O(1)
+     */
+    public ListNode mergeTwoListsLoop(ListNode l1, ListNode l2) {
+        ListNode newList = new ListNode(-1);
+        ListNode tail = newList;
+        while (l1 != null && l2 != null) {
+            if (l1.val < l2.val) {
+                tail.next = l1;
+                l1 = l1.next;
+            } else {
+                tail.next = l2;
+                l2 = l2.next;
+            }
+            tail = tail.next;
+        }
+
+        tail.next = l1 == null ? l2 : l1;
+        return newList.next;
     }
 
     /**
@@ -29,7 +76,7 @@ public class Solution {
         while (list1 != null || list2 != null) {
             //  如果只剩单个列表，直接复制
             if (list1 == null) {
-                if(newList == null){
+                if (newList == null) {
                     newList = list2;
                     break;
                 }
@@ -37,7 +84,7 @@ public class Solution {
                 break;
             }
             if (list2 == null) {
-                if(newList == null){
+                if (newList == null) {
                     newList = list1;
                     break;
                 }
