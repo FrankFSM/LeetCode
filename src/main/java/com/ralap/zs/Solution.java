61111cefff6b113b030fafb1de0bf8be35f05730
ralaphao <ralaphao@tencent.com>
2022-06-03 10:15:27 +0800

    fix: 调整目录结构

--- src/main/java/com/ralap/zs/Solution.java
+++ src/main/java/com/ralap/zs/Solution.java
@@ -1,8 +1,6 @@
 package com.ralap.zs;
 
 import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Map;
 
 /**
  * 1.获取只有一个出现奇数的数
@@ -12,8 +10,8 @@ public class Solution {
 
     public static void main(String[] args) {
         Solution solution = new Solution();
-        int result = solution.js(new int[]{7,7,7,8,8,9,9});
-        solution.js2(new int[]{7,7,7,8,8,8,9,9});
+        int result = solution.js(new int[]{7, 7, 7, 8, 8, 9, 9});
+        solution.js2(new int[]{7, 7, 7, 8, 8, 8, 9, 9});
         System.out.println(result);
     }
 
@@ -22,9 +20,9 @@ public class Solution {
     }
 
     public void js2(int[] nums) {
-         int eor = Arrays.stream(nums).reduce(0, (a, b) -> a ^ b);
-         int right = eor * (eor -1);
-         int z = Arrays.stream(nums).filter(item -> ((item & right) == 0)).reduce(0,(a,b) -> a ^ b);
-        System.out.println("a: " + z  + ", b: " + (eor ^ z));
+        int eor = Arrays.stream(nums).reduce(0, (a, b) -> a ^ b);
+        int right = eor * (eor - 1);
+        int z = Arrays.stream(nums).filter(item -> ((item & right) == 0)).reduce(0, (a, b) -> a ^ b);
+        System.out.println("a: " + z + ", b: " + (eor ^ z));
     }
 }
