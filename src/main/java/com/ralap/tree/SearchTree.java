137923345ce55f9903b6e4e6bf4006c1d4a61c25
ralap <hao_jixiang@163.com>
2022-10-13 19:44:03 +0800

    fix: searche tree

--- src/main/java/com/ralap/tree/SearchTree.java
+++ src/main/java/com/ralap/tree/SearchTree.java
@@ -1,7 +1,51 @@
 package com.ralap.tree;
 
+import com.ralap.comm.TreeNode;
+
 /**
  * 二叉搜索树
  */
 public class SearchTree {
+    private TreeNode root;
+
+    public TreeNode getRoot() {
+        return root;
+    }
+
+    /**
+     * 插入
+     *
+     * @param value
+     * @return
+     */
+    public boolean insert(int value) {
+        TreeNode newNode = new TreeNode(value);
+        // 跟节点
+        if (this.root == null) {
+            this.root = newNode;
+            return true;
+        }
+
+        TreeNode parent = null;
+        TreeNode curr = root;
+        while (curr != null) {
+            // 已经存在，不符合二叉搜索树的要求
+            if (curr.val == value) {
+                return false;
+            } else if (value < curr.val) {
+                parent = curr;
+                curr = curr.left;
+            } else {
+                parent = curr;
+                curr = curr.right;
+            }
+        }
+
+        if (value < parent.val) {
+            parent.left = newNode;
+        }else {
+            parent.right = newNode;
+        }
+        return true;
+    }
 }
---
+++ src/test/java/com/ralap/tree/SearchTreeTest.java
@@ -0,0 +1,21 @@
+package com.ralap.tree;
+
+import com.ralap.comm.TreeNode;
+import junit.framework.TestCase;
+
+public class SearchTreeTest extends TestCase {
+
+    public void testInsert() {
+        SearchTree st = new SearchTree();
+        st.insert(6);
+        st.insert(4);
+        st.insert(9);
+        st.insert(3);
+        st.insert(5);
+        st.insert(7);
+        st.insert(11);
+        st.insert(2);
+        TreeNode root  = st.getRoot();
+        TreeNode.midTraversal(root);
+    }
+}
