c539a15d2b7638b7c783862e9455aec029a9e257
ralaphao <ralaphao@tencent.com>
2022-11-11 22:24:01 +0800

    feat: 007

---
+++ src/test/java/com/ralap/_20/_7_IntegerReverseTest.java
@@ -0,0 +1,16 @@
+package com.ralap._20;
+
+import junit.framework.TestCase;
+
+public class _7_IntegerReverseTest extends TestCase {
+
+    public void testSolution() {
+        _7_IntegerReverse ir = new _7_IntegerReverse();
+        System.out.println(ir.solution(1534236469));
+    }
+
+    public void testMathSolution() {
+        _7_IntegerReverse ir = new _7_IntegerReverse();
+        System.out.println(ir.mathSolution(1534236469));
+    }
+}
