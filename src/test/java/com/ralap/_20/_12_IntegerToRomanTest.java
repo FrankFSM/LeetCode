caad4f5e1d3bfdafd49f7166471aeb50b30b5a62
ralaphao <ralaphao@tencent.com>
2022-11-14 08:28:31 +0800

    feat: 011\012

---
+++ src/test/java/com/ralap/_20/_12_IntegerToRomanTest.java
@@ -0,0 +1,11 @@
+package com.ralap._20;
+
+import junit.framework.TestCase;
+
+public class _12_IntegerToRomanTest extends TestCase {
+
+    public void testSolution() {
+        _12_IntegerToRoman itr = new _12_IntegerToRoman();
+        System.out.println(itr.solution(3999));
+    }
+}
