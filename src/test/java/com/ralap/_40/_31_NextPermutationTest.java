3eb14fd0681964e1f3e0d7387dc91f541b80da44
ralaphao <ralaphao@tencent.com>
2022-11-26 12:24:52 +0800

    feat: 031

---
+++ src/test/java/com/ralap/_40/_31_NextPermutationTest.java
@@ -0,0 +1,16 @@
+package com.ralap._40;
+
+import junit.framework.TestCase;
+
+public class _31_NextPermutationTest extends TestCase {
+
+    public void testSolution() {
+        _31_NextPermutation np = new _31_NextPermutation();
+        np.solution(new int[]{2,2,7,5,4,3,2,2});
+    }
+
+    public void testSolution_v2() {
+        _31_NextPermutation np = new _31_NextPermutation();
+        np.solution_v2(new int[]{3,2,1});
+    }
+}
