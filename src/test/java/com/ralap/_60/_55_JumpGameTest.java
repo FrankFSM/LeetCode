9c24c0cece6c0b2e8aa348d4a434f86a257b4b51
ralaphao <ralaphao@tencent.com>
2022-12-16 21:25:34 +0800

    feat: 055

---
+++ src/test/java/com/ralap/_60/_55_JumpGameTest.java
@@ -0,0 +1,23 @@
+package com.ralap._60;
+
+import junit.framework.TestCase;
+
+public class _55_JumpGameTest extends TestCase {
+
+    public void testSolution() {
+        _55_JumpGame jg = new _55_JumpGame();
+        System.out.println(jg.solution(new int[]{0}));
+        System.out.println(jg.solution(new int[]{2,3,1,1,4}));
+        System.out.println(jg.solution(new int[]{3,2,1,0,4}));
+        System.out.println(jg.solution(new int[]{2,0,0}));
+
+    }
+
+    public void testSolution2() {
+        _55_JumpGame jg = new _55_JumpGame();
+        System.out.println(jg.solution2(new int[]{0}));
+        System.out.println(jg.solution2(new int[]{2,3,1,1,4}));
+        System.out.println(jg.solution2(new int[]{3,2,1,0,4}));
+        System.out.println(jg.solution2(new int[]{2,0,0}));
+    }
+}
