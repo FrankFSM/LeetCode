889e8a4cc7fac527feec3e02f7caa0267c120172
ralaphao <ralaphao@tencent.com>
2022-12-22 22:50:58 +0800

    feat: 059

---
+++ src/test/java/com/ralap/_60/_59_SpiralMatrix2Test.java
@@ -0,0 +1,18 @@
+package com.ralap._60;
+
+import com.ralap.comm.PrintUtils;
+import junit.framework.TestCase;
+
+public class _59_SpiralMatrix2Test extends TestCase {
+
+    public void testSolution() {
+        _59_SpiralMatrix2 sm = new _59_SpiralMatrix2();
+        PrintUtils.printTwoDimensionalArray(sm.solution(3), ",");
+        PrintUtils.printTwoDimensionalArray(sm.solution(1), ",");
+        PrintUtils.printTwoDimensionalArray(sm.solution(0), ",");
+        PrintUtils.printTwoDimensionalArray(sm.solution(4), ",");
+
+
+
+    }
+}
