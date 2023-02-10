8c1a9e3af1452f4282c77d70db303c0115a2ce29
ralaphao <ralaphao@tencent.com>
2023-02-10 11:08:03 +0800

    feat: 081

---
+++ src/main/java/com/ralap/_100/_81_SearchInRotatedSortedArray2.java
@@ -0,0 +1,60 @@
+package com.ralap._100;
+
+import javax.xml.stream.XMLInputFactory;
+
+/**
+ * 81. 搜索旋转排序数组 II
+ * <p>
+ * 已知存在一个按非降序排列的整数数组 nums ，数组中的值不必互不相同。
+ * <p>
+ * 在传递给函数之前，nums 在预先未知的某个下标 k（0 <= k < nums.length）上进行了 旋转 ，使数组变为 [nums[k], nums[k+1], ..., nums[n-1], nums[0], nums[1], ..., nums[k-1]]（下标 从 0 开始 计数）。例如， [0,1,2,4,4,4,5,6,6,7] 在下标 5 处经旋转后可能变为 [4,5,6,6,7,0,1,2,4,4] 。
+ * <p>
+ * 给你 旋转后 的数组 nums 和一个整数 target ，请你编写一个函数来判断给定的目标值是否存在于数组中。如果 nums 中存在这个目标值 target ，则返回 true ，否则返回 false 。
+ * <p>
+ * 你必须尽可能减少整个操作步骤。
+ * <p>
+ *  
+ * <p>
+ * 示例 1：
+ * <p>
+ * 输入：nums = [2,5,6,0,0,1,2], target = 0
+ * 输出：true
+ * 示例 2：
+ * <p>
+ * 输入：nums = [2,5,6,0,0,1,2], target = 3
+ * 输出：false
+ * <p>
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode.cn/problems/search-in-rotated-sorted-array-ii
+ */
+public class _81_SearchInRotatedSortedArray2 {
+    public boolean solution(int[] nums, int target) {
+        int len = nums.length;
+        int left = 0;
+        int right = len - 1;
+        while (left <= right) {
+            int mid = (right + left) / 2;
+            if (nums[mid] == target) {
+                return true;
+            }
+            if (nums[left] == nums[mid] && nums[mid] == nums[right]) { // 特殊情况(左右两边相同)
+                right--;
+                left++;
+            } else if (nums[left] <= nums[mid]) {                      // 左边有序
+                if (target < nums[mid] && target >= nums[left]) {
+                    right = mid - 1;
+                } else {
+                    left = mid + 1;
+                }
+            } else {                                                    // 右边有序
+                if (target <= nums[right] && target > nums[mid]) {
+                    left = mid + 1;
+                } else {
+                    right = mid - 1;
+                }
+            }
+        }
+        return false;
+    }
+}
