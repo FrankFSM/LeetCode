8990ac57ceb2d60bae6a2655876de95a274cf983
ralaphao <ralaphao@tencent.com>
2023-02-11 10:16:35 +0800

    feat: 82

---
+++ src/test/java/com/ralap/_100/_82_RemoveDuplicatesFromSortedList2Test.java
@@ -0,0 +1,30 @@
+package com.ralap._100;
+
+import com.ralap.comm.ListNode;
+import junit.framework.TestCase;
+
+public class _82_RemoveDuplicatesFromSortedList2Test extends TestCase {
+
+    public void testSolution() {
+        _82_RemoveDuplicatesFromSortedList2 rNode = new _82_RemoveDuplicatesFromSortedList2();
+        ListNode node1 = new ListNode(1);
+        ListNode node2 = new ListNode(2);
+        ListNode node3 = new ListNode(3);
+        ListNode node4 = new ListNode(3);
+        ListNode node5 = new ListNode(4);
+        ListNode node6 = new ListNode(4);
+        ListNode node7 = new ListNode(5);
+        ListNode node8 = new ListNode(5);
+
+        node1.next = node2;
+        node2.next = node3;
+        node3.next = node4;
+        node4.next = node5;
+        node5.next = node6;
+        node6.next = node7;
+        node7.next = node8;
+
+        ListNode solution = rNode.solution(node1);
+        System.out.println(solution);
+    }
+}
