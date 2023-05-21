c87e0a4c6e5ae0d720b534bd41c0a09d2c83ba7b
ralaphao <ralaphao@tencent.com>
2023-05-21 10:55:07 +0800

    faet: 123

---
+++ src/test/java/com/ralap/_0140/_123_BestTimeToBuyAndSellStockIiiTest.java
@@ -0,0 +1,11 @@
+package com.ralap._0140;
+
+import junit.framework.TestCase;
+
+public class _123_BestTimeToBuyAndSellStockIiiTest extends TestCase {
+
+    public void testMaxProfit() {
+        _123_BestTimeToBuyAndSellStockIii solution = new _123_BestTimeToBuyAndSellStockIii();
+        System.out.println(solution.maxProfit(new int[]{3, 3, 5, 0, 0, 3, 1, 4}));
+    }
+}
